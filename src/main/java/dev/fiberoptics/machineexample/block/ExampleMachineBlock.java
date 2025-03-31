d34f283f955d947efd48f2a2007689ae6d8fbc5d
Jaaon <jingxuanshan@hotmail.com>
2025-03-31 12:33:12 -0700

    something

--- src/main/java/dev/fiberoptics/machineexample/block/ExampleMachineBlock.java
+++ src/main/java/dev/fiberoptics/machineexample/block/ExampleMachineBlock.java
@@ -2,14 +2,31 @@ package dev.fiberoptics.machineexample.block;
 
 import dev.fiberoptics.machineexample.block.entity.ExampleMachineBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 public class ExampleMachineBlock extends BaseEntityBlock {
 
-    protected ExampleMachineBlock() {
+    public ExampleMachineBlock() {
         super(Properties.of());
+    }
+
+    @Override
+    public void onRemove(BlockState blockState, Level level, BlockPos pos, BlockState newState, boolean isMoving) {
+        if(newState.getBlock() != blockState.getBlock()) {
+            if(level.getBlockEntity(pos)instanceof ExampleMachineBlockEntity blockEntity) {
+                blockEntity.drop();
+            }
+        }
+        super.onRemove(blockState, level, pos, newState, isMoving);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState blockState) {
+        return RenderShape.MODEL;
     }
 
     @Override
--- src/main/java/dev/fiberoptics/machineexample/block/entity/ExampleMachineBlockEntity.java
+++ src/main/java/dev/fiberoptics/machineexample/block/entity/ExampleMachineBlockEntity.java
@@ -1,11 +1,58 @@
 package dev.fiberoptics.machineexample.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.Containers;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.plaf.basic.BasicComboBoxUI;
 
 public class ExampleMachineBlockEntity extends BlockEntity {
+
+    private final ItemStackHandler inventory = new ItemStackHandler(1);
+    private final LazyOptional<IItemHandler> inventoryCap = LazyOptional.of(() -> inventory);
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if(cap == ForgeCapabilities.ITEM_HANDLER) {
+            if(side == Direction.UP || side == Direction.WEST || side == Direction.DOWN) {
+                return inventoryCap.cast();
+            }
+        }
+        return super.getCapability(cap, side);
+    }
+
+    public void drop() {
+        SimpleContainer container = new SimpleContainer(inventory.getSlots());
+        for (int i = 0; i < inventory.getSlots(); i++) {
+            container.setItem(i, inventory.getStackInSlot(i));
+        }
+        Containers.dropContents(this.level, this.worldPosition, container);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        tag.put("inventory",inventory.serializeNBT());
+        super.saveAdditional(tag);
+    }
+
+    @Override
+    public void load(CompoundTag tag) {
+        inventory.deserializeNBT(tag.getCompound("inventory"));
+        super.load(tag);
+    }
 
     public ExampleMachineBlockEntity(BlockPos pos, BlockState blockState) {
         super(ModBlockEntities.MACHINE_BE.get(), pos, blockState);
