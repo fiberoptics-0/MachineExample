67d993334c246b8095781866db1cf1b993839a9b
Jaaon <jingxuanshan@hotmail.com>
2025-03-31 10:21:06 -0700

    Initial commit

---
+++ src/main/java/dev/fiberoptics/machineexample/MachineExample.java
@@ -0,0 +1,15 @@
+package dev.fiberoptics.machineexample;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(MachineExample.MODID)
+public class MachineExample {
+
+    public static final String MODID = "machineexample";
+
+    public MachineExample() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    }
+}
